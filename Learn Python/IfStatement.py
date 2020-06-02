be52cbad981d06692634060d1efdda7f1db0495c
Shahzaib <shahzaibux@gmail.com>
2020-06-02 18:33:19 +0500

    Created IF Statement

---
+++ Learn Python/IfStatement.py
@@ -0,0 +1,13 @@
+a = 100
+b = 200
+
+if a < b : # return True
+    print("a is less than b")
+    print("Hello A")
+    print("Hello B")
+print("Hello python")
+
+
+if b < a : # return False
+    print("B is less than A") # this block will not be executed
+print("Pakistan") # print this
