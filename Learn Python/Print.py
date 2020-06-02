bd330d6898cac97dc257087a5500f97349d2273d
Shahzaib <shahzaibux@gmail.com>
2020-06-02 17:13:50 +0500

    Learn Python

---
+++ Learn Python/Print.py
@@ -0,0 +1,20 @@
+print(1000)
+
+print(123, 12.3) #integer and floating point
+
+print("Python") #string
+
+print("123 Pakistan") #string
+
+print("Pakistan", "Python", "Piaic", sep=" - ") #seprator
+
+print("Hello World 1") # by default end \n
+print("Hello World 2") # by default end \n
+print("Hello World 3") # by default end \n
+
+
+print("Hello World 1", end="\t\t") # escape charactor
+print("Hello World 2", end="\t\t") # escape charactor
+print("Hello World 3")
+
+
