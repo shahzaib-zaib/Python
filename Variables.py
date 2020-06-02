b140abda54f69e0f750679a17111f9ba16168374
Shahzaib <shahzaibux@gmail.com>
2020-06-02 18:07:13 +0500

    Created Variables

---
+++ Variables.py
@@ -0,0 +1,31 @@
+a = 10
+# a is variable name(identifier)
+# = assignment operator
+# 10 value (interger value)
+
+print(a) # type int
+
+a = 10.6
+
+print(a) # type float
+
+
+city = "Karachi"
+print(city) # type string
+
+
+city = 25
+print(city) # type int
+
+
+student_name = "Shahzaib"
+student_age = 30
+
+print(student_name + student_age)
+
+
+
+num1 = 100
+num2 = 200
+sum = num1 + num2
+print(sum) # 300
