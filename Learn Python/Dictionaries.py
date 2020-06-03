44b74ed325b0604451606ec6e48c33f7b7c73292
Shahzaib <shahzaibux@gmail.com>
2020-06-03 13:09:16 +0500

    Created Dictionary

---
+++ Learn Python/Dictionaries.py
@@ -0,0 +1,11 @@
+# myDict = {key:value, key:value, key:value}
+
+myDict = {"name":"Shahzaib", "age":30, "gender": "Male"}
+
+len(myDict) # 3
+
+# Adding a new key to an exiting Dictionary
+
+myDict["email"] = "shahzaibux@gmail.com"
+
+myDict # {"name":"Shahzaib", "age":30, "gender": "Male", "email":"shahzaibux@gmail.com"}
